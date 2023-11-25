70ecafa781b48cd602b4f7b4c556cdd5ad9fe35f
O Hung Lun <hunglun.o@gmail.com>
2023-11-25 02:44:29 +0000

    add App.js and package.json

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import Profile from './components/Profile'; // Adjust the path based on your file structure
 import PortfolioSection from './components/PortfolioSection'; // Import the PortfolioSection component
-
+import SingaporeLocationTimeline from './components/SingaporeLocationTimeline';
+import TimelineComponent from './components/TimelineComponent';
 function App() {
   return (
     <div className="App">
-      <Profile />
-      <PortfolioSection />
+      <TimelineComponent />
+      <SingaporeLocationTimeline />
+      <div style={{ display: 'none' }}>
+        <Profile />
+        <PortfolioSection />
+      </div>
     </div>
   );
 }
